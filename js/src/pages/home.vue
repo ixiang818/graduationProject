8c802244d2e799084fee0027c7f5486e51dcb26d
敖翔云 <1162102473@qq.com>
2021-03-09 23:54:33 +0800

    develop commit

--- js/src/pages/home.vue
+++ js/src/pages/home.vue
@@ -1,6 +1,7 @@
 <template>
   <f7-page class="home-page" @page:beforeout="onPageBeforeOut" @page:afterin="onPageAfterIn">
     <f7-navbar title="Home"></f7-navbar>
+    <f7-button fill>Button</f7-button>
     <div>hello world</div>
   </f7-page>
 </template>
